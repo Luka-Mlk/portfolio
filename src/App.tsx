c282d774335f38603b4fd487a1c5b7754c109791
Luka-Mlk <luka.milik1@gmail.com>
2025-07-15 01:08:54 +0200

    added layout component to have standardized layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "@pages/Home";
+import { Layout } from "@components/Layout/Layout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Layout />}> */}
-        <Route index element={<Home />} />
-        {/* <Route path="blogs" element={<Blogs />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NoPage />} />
-        </Route> */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
