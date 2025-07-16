8e2973fd57410281763065039697e159ae17c443
Luka-Mlk <luka.milik1@gmail.com>
2025-07-16 02:38:42 +0200

    added hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+const Hero: React.FC = () => {
+  return (
+    <>
+      <div className="hero">
+        <h1>This is content from the hero component</h1>
+      </div>
+    </>
+  );
+};
+
+export { Hero };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { Hero } from "@components/Hero/Hero";
 import Parallax from "@components/Parallax/Parallax";
 
 const Home: React.FC = () => {
   return (
     <>
       <Parallax />
+      <Hero />
     </>
   );
 };
