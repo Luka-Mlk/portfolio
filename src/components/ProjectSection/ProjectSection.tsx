5ef2c0aec4ed69ff8d488e6377ce45d2f424cf80
Luka-Mlk <luka.milik1@gmail.com>
2025-07-18 02:21:54 +0200

    basic content changes to project section, adittion of title

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -1,5 +1,13 @@
+import { StockTrust } from "@components/Projects/StockTrust/StockTrust";
+import "./ProjectSection.css";
+
 const ProjectSection: React.FC = () => {
-  return <></>;
+  return (
+    <div className="project__section">
+      <h2>Here's some of my projects</h2>
+      <StockTrust />
+    </div>
+  );
 };
 
 export { ProjectSection };
