41120d9ed9ca22441511ed45819c99b681eecd88
Luka-Mlk <luka.milik1@gmail.com>
2025-07-18 02:24:35 +0200

    Added project section to layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { LeadSection } from "@components/LeadSection/LeadSection";
 import Parallax from "@components/Parallax/Parallax";
+import { ProjectSection } from "@components/ProjectSection/ProjectSection";
 
 const Home: React.FC = () => {
   return (
     <>
       <Parallax />
       <LeadSection />
+      <ProjectSection />
     </>
   );
 };
