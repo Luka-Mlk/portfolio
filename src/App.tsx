027f604788197aee7193f849ea04c38ac66e83e7
Luka-Mlk <luka.milik1@gmail.com>
2025-07-19 02:49:20 +0200

    test changes to vite and routing component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/portfolio" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
